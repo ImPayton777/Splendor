75aff244b1a72ca28eecf2c07ccacda74d9ca63a
Alex Reiffer <awr7648@gmail.com>
2025-02-10 09:17:51 -0500

    reated ccard class

---
+++ card.py
@@ -0,0 +1,15 @@
+import os
+import pygame as pg
+
+class Card(pg.sprite.Sprite):
+
+    points = 0
+
+    def __init__(self, coord):
+        super(Card, self).__init__()
+
+    def draw(self, screen):
+
+    def setPoints(self, points):
+
+    def getPoints(self, points):
