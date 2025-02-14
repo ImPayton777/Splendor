c6160b910c8457f3c8416ebff29fa8d07de50045
Alex Reiffer <awr7648@gmail.com>
2025-02-14 11:54:22 -0500

    Taken and card now load images

--- card.py
+++ card.py
@@ -7,6 +7,8 @@ class Card(pg.sprite.Sprite):
     tokenType = 0
     #Advancement points
     points = 0
+    #Card level
+    level = 1
     #Cost of each type
     whiteCost = 0
     blueCost = 0
@@ -15,8 +17,27 @@ class Card(pg.sprite.Sprite):
     brownCost = 0
 
     #creates a card
-    def __init__(self, coord):
+    def __init__(self, coord, white, blue, green, red, brown, points, gem, lvl):
         super(Card, self).__init__()
+        self.whiteCost = white
+        self.blueCost = blue
+        self.greenCost = green
+        self.redCost = red
+        self.brownCost = brown
+        self.points = points
+        if lvl == 1:
+            self.image = pg.image.load(os.path.join('assets', 'lvl1Card.png')).convert_alpha()
+        elif lvl == 2:
+            self.image = pg.image.load(os.path.join('assets', 'lvl2Card.png')).convert_alpha()
+        elif lvl == 3:
+            self.image = pg.image.load(os.path.join('assets', 'lvl3Card.png')).convert_alpha()
+        else:
+            raise ValueError("Not a card level")
+        self.tokenType = gem
+        self.lvl = lvl
+        self.rect = self.image.get_rect()
+        self.rect.x = coord[0]
+        self.rect.y = coord[1]
 
     #draws a card
     def draw(self, screen):
@@ -39,3 +60,5 @@ class Card(pg.sprite.Sprite):
     def getCost(self):
         return [self.whiteCost, self.blueCost, self.greenCost, self.redCost, self.brownCost]
 
+    def get cardType:
+        return tokenType
--- token.py
+++ token.py
@@ -6,8 +6,20 @@ class Token(pg.sprite.Sprite):
     #Type codes: 0 = White, 1 = Blue, 2 = Green, 3 = Red, 4 = Brown
     tokenType = 0
     #creates a token
-    def __init__(self, coord):
+    def __init__(self, coord, gem):
         super(Token, self).__init__()
+        if gem == 0:                                                                         self.image = pg.image.load(os.path.join('assets', 'whiteToken.png')).convert_alpha()
+        elif gem == 1:
+            self.image = pg.image.load(os.path.join('assets', 'blueToken.png')).convert_alpha()
+        elif gem == 2:
+            self.image = pg.image.load(os.path.join('assets', 'greenToken.png')).convert_alpha()
+        elif gem == 3:
+            self.image = pg.image.load(os.path.join('assets', 'redToken.png')).convert_alpha()
+        elif gem == 4:
+            self.image = pg.image.load(os.path.join('assets', 'brownToken.png')).convert_alpha()
+        else:
+            raise ValueError("Not a gem type")
+        self.tokenType = gem
 
     #draws a token
     def draw(self, screen):
