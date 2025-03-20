41194bb7e09791e2bd34c2037234f866633e1a56
Payton Wilder <paytonwilder@Paytons-MacBook-Air-2.local>
2025-03-20 10:38:00 -0400

    Actually added the noble class

---
+++ noble.py
@@ -0,0 +1,38 @@
+import os
+import pygame as pg
+
+class Noble():
+    def __init__(self, coord, white, blue, green, red, brown, points):
+        self.whiteCost = white
+        self.blueCost = blue
+        self.greenCost = green
+        self.redCost = red
+        self.brownCost = brown
+        self.points = points
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = coord[0]
+        self.rect.y = coord[1]
+
+    def getCords(self):
+        return [self.rect.x, self.rect.y]
+
+    def update(self, deltax, deltay = 0):
+        self.rect.x += deltax
+        self.rect.y += deltay
+
+    def getPoints(self):        
+        return self.points
+
+    def setPoints(self, points):
+        self.points = points
+
+    def getCost(self):
+        return [self.whiteCost, self.blueCost, self.greenCost, self.redCost, self.brownCost]
+
+    def setCost(self, white, blue, green, red, brown):
+        self.whiteCost = white
+        self.blueCost = blue
+        self.greenCost = green
+        self.redCost = red
+        self.brownCost = brown
