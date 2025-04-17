097d1a53714a32247639f0036a2632ad4e049856
Payton Wilder <paytonwilder@Paytons-MacBook-Air-2.local>
2025-04-17 11:09:42 -0400

    Added more tests to test_card

--- test_card.py
+++ test_card.py
@@ -25,19 +25,27 @@ class testCards(unittest.TestCase):
         k = Card((0,0),0,1,1,1,1,0,0,1)
         k.update(5,7)
         self.assertEqual(k.rect.x,5)
-        self.assertEqual(k.rect.y,7)   
+        self.assertEqual(k.rect.y,7)
+
+    def test_move(self):
+        k = Card((122,456),0,1,1,1,1,0,0,1)
+        k.move(0,0)
+        self.assertEqual(k.rect.x,0)
+        self.assertEqual(k.rect.y,0)
 
     #Tests to see if getPoints properly returns card point value.
     def test_getPoints(self):
         k = Card((0,0),0,1,1,1,1,0,0,1)
         points = k.getPoints()
         self.assertEqual(points,0)
-
+    
+    """
     #Tests to see if the setPoints method sets it's points to given value.
     def test_setPoints(self):
         k = Card((0,0),0,1,1,1,1,0,0,1)
         k.setPoints(5)
         self.assertEqual(k.getPoints(),5)
+    """
 
     #Tests to see if getCardType properly returns the card's type.
     def test_getCardType(self):
@@ -56,6 +64,16 @@ class testCards(unittest.TestCase):
         k = Card((0,0),0,1,1,1,1,0,0,1)
         k.setCost(1,2,3,4,5)
         self.assertEqual(k.getCost(),[1,2,3,4,5])
+
+    def test_flip(self):
+        k = Card((0,0),0,1,1,1,1,0,0,1)
+        k.flip(100,100)
+        self.assertEqual(k.rect.x,100)
+        self.assertEqual(k.rect.y,100)
+
+    def test_getLvl(self):
+        k = Card((0,0),0,1,1,1,1,0,0,1)
+        self.assertEqual(k.getLvl(),1)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
